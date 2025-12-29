04961cad073db5dbeebe00eff6047917a82d4691
bomberosc80-app <sergionovo88@gmail.com>
2025-12-28 22:27:45 -0300

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 // 🔗 CAMBIÁ ESTAS URL POR LAS DE TUS REPOS
-const URL_BOMBERO = "https://TUUSUARIO.github.io/calificaciones-app/";
-const URL_JEFES   = "https://TUUSUARIO.github.io/calificacionesc80/";
+const URL_BOMBERO = "https://bomberosc80-app.github.io/calificaciones-app/";
+const URL_JEFES   = "https://bomberosc80-app.github.io/calificacionesc80/";
 
 function irBombero() {
   window.location.href = URL_BOMBERO;
