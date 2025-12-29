2da8b48c6595ad86b9ad7c2ce5d6b4b37a462912
bomberosc80-app <sergionovo88@gmail.com>
2025-12-28 22:14:01 -0300

    Create app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+// 🔗 CAMBIÁ ESTAS URL POR LAS DE TUS REPOS
+const URL_BOMBERO = "https://TUUSUARIO.github.io/calificaciones-app/";
+const URL_JEFES   = "https://TUUSUARIO.github.io/calificacionesc80/";
+
+function irBombero() {
+  window.location.href = URL_BOMBERO;
+}
+
+function irJefe() {
+  window.location.href = URL_JEFES;
+}
